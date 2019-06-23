ecde5f9a1202fca7298d6e054f5bb893791889e9
Austin Woo <wooaustin0@gmail.com>
2019-06-23 14:14:11 -0700

    Initial Commit

---
+++ src/main/java/com/example/larktbin/LarktbinApplication.java
@@ -0,0 +1,13 @@
+package com.example.larktbin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarktbinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarktbinApplication.class, args);
+	}
+
+}
